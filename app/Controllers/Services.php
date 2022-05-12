c824ccc078a787ce8eae946cd89bcbc99ba2780b
erick243cd <erickbanze.develo@gmail.comd>
2022-05-11 23:36:44 -0700

    view function

--- app/Controllers/Services.php
+++ app/Controllers/Services.php
@@ -3,11 +3,31 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\ServiceModel;
+use Config\UserAgents;
 
 class Services extends BaseController
 {
     public function index()
     {
         //
+    }
+
+    public function service($serviceSlug)
+    {
+        $service =$this->serviceModel->asObject()->where('serviceSlug', $serviceSlug)->first();
+        if (!empty($service)) {
+            $page = "service_view";
+            $agent = $this->request->getUserAgent();
+            $data = [
+                'service' => $service,
+                'links' => headerData($page, localLang()),
+                'isMobile' => $agent->isMobile(),
+            ];
+
+            return view('pages/' . localLang() . '/' . $page, $data);
+        } else {
+            return view('errors/404');
+        }
     }
 }
