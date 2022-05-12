fee68b572b0b8f75340f4366ca0d43b414da0268
erick243cd <erickbanze.develo@gmail.comd>
2022-05-11 23:37:58 -0700

    GetLocal lang function

--- app/Helpers/custom_helper.php
+++ app/Helpers/custom_helper.php
@@ -21,14 +21,23 @@ function headerData(?string $page, ?string $lang)
             <meta name="keywords" content="Afrinewsoft, Agence digital RDC, Startup Numérique de Lubumbashi">
             <meta name="author" content="Afrinewsoft Erick Banze">'
         ],
-        'home'=> ($lang === 'fr') ? 'Accueil' : 'Home',
-        'about'=> ($lang === 'fr') ? 'A propos' : 'About Us',
-        'services'=> ($lang === 'fr') ? 'Nos Services' : 'Our Services Us',
-        'contact'=> ($lang === 'fr') ? 'Contact' : 'Contact Us',
-        'gallery'=> ($lang === 'fr') ? 'Galerie' : 'Gallery',
+        'home' => ($lang === 'fr') ? 'Accueil' : 'Home',
+        'about' => ($lang === 'fr') ? 'A propos' : 'About Us',
+        'services' => ($lang === 'fr') ? 'Nos Services' : 'Our Services Us',
+        'contact' => ($lang === 'fr') ? 'Contact' : 'Contact Us',
+        'gallery' => ($lang === 'fr') ? 'Galerie' : 'Gallery',
     ];
 
     return (object)$data;
 }
 
+//Get agent long
+function localLang()
+{
+    $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+    $acceptLang = ['fr', 'en'];
 
+    $lang = in_array($lang, $acceptLang) ? $lang : 'fr';
+
+    return $lang;
+}
